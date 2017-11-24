07bc44011ba472f343cc5a0d556155fcb9161e13
Phil Sparkes <phil.sparkes95@gmail.com>
2017-11-24 16:31:23 +0000

    added diffuse and specular

---
+++ COMP220/COMP220_Examples/10_Reflectance/lightingFrag.glsl
@@ -0,0 +1,20 @@
+#version 330 core
+
+in vec4 vertexColourOut;
+in vec2 vertexTextureCoordOut;
+in vec4 diffuse;
+in vec4 specular;
+in vec4 ambient;
+
+out vec4 colour;
+
+uniform vec4 fragColour=vec4(1.0,1.0,1.0,1.0);
+
+uniform sampler2D baseTexture;
+
+uniform float time=0.0f;
+
+void main()
+{
+    colour = ambient+(diffuse*texture(baseTexture, vertexTextureCoordOut))+specular;
+}

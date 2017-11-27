17e58565ead6d7e4950b23471feb03301edf84a9
Phil Sparkes <phil.sparkes95@gmail.com>
2017-11-27 14:09:20 +0000

    update

---
+++ COMP220/COMP220_Examples/13_BulletPhysics/Texture.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "SDL_image.h"
+#include <GL\glew.h>
+#include <SDL_opengl.h>
+
+#include <string>
+
+GLuint loadTextureFromFile(const std::string& filename);
+
+GLuint createTexture(int width, int height);
---
+++ COMP220/COMP220_Examples/13_BulletPhysics/textureFrag.glsl
@@ -0,0 +1,18 @@
+#version 330 core
+
+in vec4 vertexColourOut;
+in vec2 vertexTextureCoordOut;
+
+out vec4 colour;
+
+uniform vec4 fragColour=vec4(1.0,1.0,1.0,1.0);
+
+uniform sampler2D baseTexture;
+
+uniform float time=0.0f;
+
+void main()
+{
+    colour=texture2D(baseTexture,vertexTextureCoordOut);
+
+}
